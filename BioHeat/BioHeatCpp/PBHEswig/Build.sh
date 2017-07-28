d5600b6f67d1d578f6648fee7c6db189eeeebd4c
caskeylab@winterfell-linux <caskeylab@winterfell-linux>
2017-07-28 14:26:49 -0500

    .so extension for linux/mac. Moved ablation_utils to myPy

--- BioHeat/BioHeatCpp/PBHEswig/Build.sh
+++ BioHeat/BioHeatCpp/PBHEswig/Build.sh
@@ -1,5 +1,5 @@
 
-export NUMPYINC=/usr/local/lib/python3.5/dist-packages/numpy/core/include
+#export NUMPYINC=/usr/local/lib/python3.5/dist-packages/numpy/core/include
 
 CXXFLAGS+="-O2 -Wall -fPIC -std=c++11" 
 
@@ -12,8 +12,6 @@ g++ -c $CXXFLAGS $PYINC -I$NUMPYINC -I"../BioHeatCpp" PBHEswig.cpp -o PBHEswig.o
 
 PYFLAGS=`python3-config --ldflags`
 
-#PYFLAGS="-L/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu -L/usr/lib -lpython3.5m -lpthread -ldl  -lutil -lm"
+g++ -shared PBHEswig_wrap.o PBHEswig.o $PYFLAGS -o _PBHEswig.so
 
-g++ -shared PBHEswig_wrap.o PBHEswig.o $PYFLAGS -o _PBHEswig.pyd
-
-mv _PBHEswig.pyd PBHEswig.py custom
+mv _PBHEswig.so PBHEswig.py custom
